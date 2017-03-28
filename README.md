9f00c789161fcf24fb8bf7f245d3d2e0401eb723
Logan Drake <lsd16@my.fsu.edu>
2017-03-28 15:33:58 +0000

    Added README.md and updated menu options

---
+++ README.md
@@ -0,0 +1,23 @@
+## Project Name:  Pandora music Application
+
+### Course Title:
+COP 2250: Problem solving with Object Oriented programming
+
+### Assignment Date:  
+March 31, 2017
+
+### Student Name:  
+Logan Drake
+
+### Project Description:
+This assignment I will be building an application that asks users to create and customize Pandora
+music playlists. 
+
+### View Project:
+(Replace this statement with your Github Page URL that was created when you 
+ published the project.)
+
+### Lessons Learned in the Assignment:
+1. This assignment taught me the basics of jQuery and how it works.
+2. This assignment taught me how to use core jQuery features like DOM manipulation, event handling, and AJAX support.
+3. This assignment taught me how to find and use useful jQuery resources. 
--- java/pandora.java
+++ java/pandora.java
@@ -8,11 +8,10 @@ public class pandora
         Scanner input = new Scanner (System.in);
         
         //Variables
+        int channel=0;
         String name ="", lname ="";
         String menuSelection ="";
-        String channel="";
-        
-        
+        String channelName ="";
         
         //Display welcome menu
         System.out.println("**************** WELCOME TO PANDORA ******************************");
@@ -23,41 +22,75 @@ public class pandora
         System.out.println("3 - Exit Pandora");
         System.out.println(""); //Blank line
         System.out.println("******************************************************************");
-        
-        System.out.println("Please enter your last name followed by your Menu Choice: "); //User enters 1,2, or 3 from welcome menu
+        System.out.println(""); //Blank line
+        System.out.println("Please enter your last name followed by your Menu Choice: "); //User enters 1, 2, or 3 from welcome menu
         
         lname = input.next(); //Read last name from keyboard
-        lname = name.toUpperCase(); //Change name to upper case
-        
-        //Pandora Menu Option #1
-        System.out.println("You have selected the CREATE NEW PANDORA CHANNEL menu item!"); //Welcome message 
-        System.out.println("Please enter the name of your new channel: "); // Display channel name in all caps
-        
-        
-        
-        System.out.println("You have succesfully created the following channel: " + name ); //Display channel name in all caps
-        System.out.println( lname + " Thank you for being a valued listener!"); //Display user's last name in all caps
-        
-        
-        
-
-        
-      
-        //Pandora Menu Option #2
-        System.out.println("You have selected the PLAY PANDORA CHANNEL menu item!");
-        System.out.println("The user: " + lname + " currently has created the following channels:");
-                                //lname  ^^^^^ needs to be in all caps
-        System.out.println("Which chanel would you like to listen to? (Enter 1, 2, 3, 4, or 5): ");
-        System.out.println("You are now listening to " + channel); //Display the chanel the user has selected
-        
-        //Pandora Menu Option #3
-        System.out.println("You have selected the EXIT PANDORA menu item!");
+        //lname = name.toUpperCase(); //Makes name all caps
+        menuSelection = input.next();
+        System.out.println("Hello, " + lname); //Should be displayed in all caps
         
         
         
         
-        
-        
+        //switch statements
+        switch (menuSelection) {
+            case "1" : //Menu #1
+            
+                System.out.println("You have selected the CREATE NEW PANDORA CHANNEL menu item!");
+                System.out.println("Please enter the name of your new channel: ");
+                
+                channelName = input.next();
+                
+                System.out.println("You have successfully created the following Pandora channel: " + channelName); //Print channel name in all caps
+                System.out.println(""); //Blank line
+                System.out.println(lname + " Thank you for being a valued listener!");
+                break;
+            
+            case "2" : //Menu #2
+            
+                System.out.println("You have selected the PLAY PANDORA CHANNEL menu item!");
+                System.out.println(""); //Blank line
+                System.out.println("The user: " + lname + " currently has created the following channels: "); //lname needs to be displayed and capitalized
+                System.out.println("1. Logic");
+                System.out.println("2. August Burns Red");
+                System.out.println("3. Kanye West");
+                System.out.println("4. Drake");
+                System.out.println("5. Rich Homie Quan");
+                
+                System.out.println("Which cannel would you like to listen to? (Enter 1, 2, 3, 4, or 5): ");
+                channel = input.nextInt(); //Read user's channel input
+                    
+                    switch (channel) {
+                        case 1 :
+                            System.out.println("You are now listening to: LOGIC");
+                            break;
+                        
+                        case 2 :
+                            System.out.println("You are now listening to: AUGUST BURNS RED");
+                            break;
+                        
+                        case 3 : 
+                            System.out.println("You are now listening to: KANYE WEST");
+                            break;
+                            
+                        case 4 :
+                            System.out.println("You are now listening to: DRAKE");
+                            break;
+                        
+                        case 5 :
+                            System.out.println("You are now listening to: RICH HOMIE QUAN");
+                            break;
+                        
+                    } //End of channel switch
+                    
+                System.out.println("Thank you " + lname + " for being a valued listener");
+                break;
+                
+            case "3" : //Menu #3
+                System.out.println("You have selected the EXIT PANDORA menu item");
+                break;
+        } //End of menu switch statement
         
         
         System.out.println("****************************GOODBYE PANDORA LISTENER************************"); //Closing message
